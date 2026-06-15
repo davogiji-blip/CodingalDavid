253f3fe632ae7609d874ff267cfb0e8e934d6452
David <davogiji@gmail.com>
2026-06-15 12:19:30 -0500

    first commit

---
+++ lesson2/snackshop.py
@@ -0,0 +1,8 @@
+snack_name="chips"
+snack_price = 1.50
+snack_quantity = 5
+is_available = True
+print(snack_name,snack_price,snack_quantity) 
+print(snack_price - 1)
+print(snack_price == 1.50)
+print(snack_name[0])
